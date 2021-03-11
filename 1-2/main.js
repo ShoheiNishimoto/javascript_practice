358c6c44808a771980290709d3b10c3d51480e4f
ShoheiNishimoto <s.nishimoto.stella@gmail.com>
2021-03-12 04:40:28 +0900

    修正漏れを訂正

--- 1-2/main.js
+++ 1-2/main.js
@@ -1,12 +1,12 @@
-function quiz() {
+function quiz () {
   let answer = true;
   
   while (answer) {
-    const reply = prompt('日本の首都は？');
+    const reply = prompt ('日本の首都は？');
     if (reply !== '東京') {
-      alert('不正解です!');
-    } else if (reply === '東京'){
-      alert('正解です!');
+      alert ('不正解です!');
+    } else if (reply === '東京') {
+      alert ('正解です!');
       answer = false;
     }
   }
