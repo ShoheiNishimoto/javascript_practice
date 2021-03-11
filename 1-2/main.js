18e6491d08383870aea0114b32a62635f55ca7bc
ShoheiNishimoto <s.nishimoto.stella@gmail.com>
2021-03-12 04:36:31 +0900

    スペースを追加を追加。演算子を修正

--- 1-2/main.js
+++ 1-2/main.js
@@ -3,9 +3,9 @@ function quiz() {
   
   while (answer) {
     const reply = prompt('日本の首都は？');
-    if(reply !== '東京'){
+    if (reply !== '東京') {
       alert('不正解です!');
-    }else if(reply == '東京'){
+    } else if (reply === '東京'){
       alert('正解です!');
       answer = false;
     }
